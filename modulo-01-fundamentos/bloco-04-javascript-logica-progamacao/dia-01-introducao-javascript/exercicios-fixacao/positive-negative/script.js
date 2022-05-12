d452e93b727af5f253d26ad4c46f2805aabd65fc
Marcos Paulo <dev.marcospereira@gmail.com>
2022-05-12 18:52:10 -0300

    Cria positive e negative exercicios fixação bloco4 dia1

---
+++ modulo-01-fundamentos/bloco-04-javascript-logica-progamacao/dia-01-introducao-javascript/exercicios-fixacao/positive-negative/script.js
@@ -0,0 +1,12 @@
+const numero = -10;
+
+switch (numero) {
+    case -10:
+        console.log("negative");
+        break;
+    case 10:
+        console.log ("positive");
+        break;
+    default:
+        console.log ("não é numero");
+}
