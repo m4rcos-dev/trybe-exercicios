5ed8ddeedf2f12cc7c49496772ba681f15b11e18
Marcos Paulo <dev.marcospereira@gmail.com>
2022-05-12 18:26:56 -0300

    Cria calculadora exercicio fixação bloco4 dia 1

---
+++ modulo-01-fundamentos/bloco-04-javascript-logica-progamacao/dia-01-introducao-javascript/exercicios-fixacao/calculadora-basica/script.js
@@ -0,0 +1,24 @@
+const a = 2;
+const b = 3;
+let resultado ;
+
+//SOMA
+resultado = a + b;
+console.log (resultado);
+
+//SUBTRAÇÃO 
+resultado = a - b;
+console.log (resultado);
+
+//MULTIPLICAÇÃO
+resultado = a * b;
+console.log (resultado);
+
+//DIVISÃO
+resultado = a / b;
+console.log (resultado);
+
+//MÓDULO
+resultado = a % b;
+console.log (resultado);
+
