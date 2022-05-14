dfdd3cbd511a4d53801d4a743ea7a576c6e68e3b
Marcos Paulo <dev.marcospereira@gmail.com>
2022-05-14 12:12:44 -0300

    Cria exercicio6 fixação 4.1

--- modulo-01-fundamentos/bloco-04-javascript-logica-progamacao/dia-01-introducao-javascript/exercicios-fixacao/5-angulos-triangulo/script.js
+++ modulo-01-fundamentos/bloco-04-javascript-logica-progamacao/dia-01-introducao-javascript/exercicios-fixacao/5-angulos-triangulo/script.js
@@ -1,6 +1,6 @@
-const angulo1 = -60;
-const angulo2 = -60;
-const angulo3 = -60;
+const angulo1 = 60;
+const angulo2 = 60;
+const angulo3 = 60;
 let somaAngulos = angulo1 + angulo2 + angulo3; // Soma todos angulos
 let angulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0; //Verifica se todos os angulos informados são maior que 0.
 
